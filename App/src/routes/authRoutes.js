d7d94a3cda7779c9cd96fdf90c724a7303063663
Abaan <sayeed.abaan@gmail.com>
2025-03-12 16:29:41 +0530

    functionalities implemented

--- App/src/routes/authRoutes.js
+++ App/src/routes/authRoutes.js
@@ -1,43 +1,65 @@
-import express from "express"
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
-import db from "../db.js"
+import express from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import db from '../db.js'
 
 const router = express.Router()
 
-router.post("/register", (req, res)=>{
-    const {username, password} = req.body
-    
+// Register a new user endpoing /auth/register
+router.post('/register', (req, res) => {
+    const { username, password } = req.body
+    // save the username and an irreversibly encrypted password
+    // save gilgamesh@gmail.com | aklsdjfasdf.asdf..qwe..q.we...qwe.qw.easd
 
-    const hashed  = bcrypt.hashSync(password, 8)
-    try{
-        const insertUser = db.prepare(`INSERT INTO users(username, password)
-        VALUES (?, ?)
-        `)
-        const result  = insertUser.run(username, hashed)
-            const defaultTodo = "HELLO!, Start writing your To-Do's"
-            const insertTodo = db.prepare(`INSERT INTO todos(user_id, task) VALUES (?, ?)`)
-            
-            insertTodo.run(result.lastInsertRowid, defaultTodo)
+    // encrypt the password
+    const hashedPassword = bcrypt.hashSync(password, 8)
 
-            const token = jwt.sign({id : result.lastInsertRowid}, process.env.JWT_SECRET, { expiresIn: "24h" })
-            
-            res.json({token})
-            
+    // save the new user and hashed password to the db
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
 
-        
-    }
-    catch (err){
+        // now that we have a user, I want to add their first todo for them
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
         console.log(err.message)
-        res.sendStatus(504)
+        res.sendStatus(503)
     }
-    
 })
 
-router.post("/login", (req,res)=>{
-    const {username, password} = req.body
-    console.log(username, password)
-    res.sendStatus(200)
+router.post('/login', (req, res) => {
+    // we get their email, and we look up the password associated with that email in the database
+    // but we get it back and see it's encrypted, which means that we cannot compare it to the one the user just used trying to login
+    // so what we can to do, is again, one way encrypt the password the user just entered
+
+    const { username, password } = req.body
+
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const user = getUser.get(username)
+
+        // if we cannot find a user associated with that username, return out from the function
+        if (!user) { return res.status(404).send({ message: "User not found" }) }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        // if the password does not match, return out of the function
+        if (!passwordIsValid) { return res.status(401).send({ message: "Invalid password" }) }
+        console.log(user)
+
+        // then we have a successful authentication
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+
 })
 
 
--- App/src/server.js
+++ App/src/server.js
@@ -1,28 +1,33 @@
-import express from "express";
-import path, {dirname} from "path"
-import { fileURLToPath } from "url";
-import authRouter from "./routes/authRoutes.js";
-import todoRouter from "./routes/todoRoutes.js"
+import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
+import authMiddleware from './middleWares/authMiddlewWare.js'
 
-    const app = express();
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename)
+const app = express()
+const PORT = process.env.PORT || 5003
 
-    const Port = process.env.Port || 5000;
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
 
-    app.use(express.static(path.join(__dirname, "../public")))
+// Middleware
+app.use(express.json())
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets / file. Any requests for the css files will be resolved to the public directory.
+app.use(express.static(path.join(__dirname, '../public')))
 
-    app.use(express.json())
+// Serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
-    app.use("/auth", authRouter)
+// Routes
+app.use('/auth', authRoutes)
+app.use('/todos', authMiddleware, todoRoutes)
 
-    app.get("/", (req,res)=>{
-        res.sendFile(path.join(__dirname, "public", "index.html"))
-    
-    })
-
-        app.listen(Port, ()=>{
-            console.log(`Server is running on ${Port}`)
-        })
-
-        
+app.listen(PORT, () => {
+    console.log(`Server has started on port: ${PORT}`)
+})
